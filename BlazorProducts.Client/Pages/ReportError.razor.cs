92cc209c632ac6447d49248cd5cea3102d5c6449
Vikram Singh Saini <vs00saini@gmail.com>
2021-04-17 21:22:03 +0530

    Module 4 - Routing in Blazor WebAssembly

---
+++ BlazorProducts.Client/Pages/ReportError.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class ReportError
+    {
+        [Parameter]
+        public int ErrorCode { get; set; }
+
+        [Parameter]
+        public string ErrorDescription { get; set; }
+
+    }
+}
